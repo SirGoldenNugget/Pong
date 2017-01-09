cacb5b23a4a1d5ba71d1d0dfbe9ec146ac041837
Minh Vu <minhd_vu@yahoo.com>
2017-01-09 16:44:20 -0500

    Readjusted File System.

---
+++ src/com/minhvu/main/Sound.java
@@ -0,0 +1,11 @@
+package com.minhvu.main;
+
+import java.applet.Applet;
+import java.applet.AudioClip;
+
+public class Sound
+{
+	public static final AudioClip BOUNCE = Applet.newAudioClip(Sound.class.getResource("bounce.wav"));
+	public static final AudioClip GAMEOVER = Applet.newAudioClip(Sound.class.getResource("gameover.wav"));
+	public static final AudioClip BACKGROUND = Applet.newAudioClip(Sound.class.getResource("background.wav"));
+}
